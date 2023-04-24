d1c9d30acfcd645e3499ec663797ea912912603c
Tobias Böhm <code@aibor.de>
2023-04-24 12:46:22 +0200

    Disable movement helpers

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/trouble.nvim", enabled = false },
+  { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
 }

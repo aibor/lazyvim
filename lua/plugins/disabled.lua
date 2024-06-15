8e1beb91d0cb9a6c81b036050cb64db17e6e61f4
Tobias Böhm <code@aibor.de>
2024-06-15 21:05:47 +0200

    Enable trouble

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
-  { "folke/trouble.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
 }
